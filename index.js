fc818940ea5b605ed2850cc37411a4686a090db9
yahapaul <papi.ansay@gmail.com>
2019-08-30 16:17:29 +0800

    validations added

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
+const dialog = require('dialog');
 const express = require('express');
 const app = express();
 const port = 3000;
 const bodyParser = require('body-parser');
 const path = require('path');
+var check = false;
+var os = require("os");
 
 app.set('views', path.join(__dirname, 'server/views'));
 app.set('view engine' ,'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static('public'));
-
+app.use(express.static('public'));	
+var accepted =[];
+var rejected =[];
 
 
 const knex  =  require ('knex')({
@@ -23,11 +27,37 @@ const knex  =  require ('knex')({
 app.post('/addIP',async(req,res) =>{
 	try{	
 		var newIP = req.body.newIP;
-		let id = await knex('IP').insert({
-			'IPadd': newIP
-		});
-		res.redirect('/');
+		var inputs = newIP.split(os.EOL);
+		console.log(inputs);
+        let IPadd =  await knex('IP').select('IPadd');
 
+        for (var x = 0; x < inputs.length; x++){
+        	inputs[0] = inputs[0].replace(/\r?\n|\r/g, "")
+        	for (var i = 0; i < IPadd.length; i++) {
+	        	if(IPadd[i].IPadd == inputs[x]){
+	        		check = true;
+        		}
+        	}
+        	if (/^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(inputs[x])){
+			 	if(check == false){
+				 	id = await knex('IP').insert({
+						'IPadd': inputs[x]
+					});
+					accepted.push(inputs[x]);
+					res.redirect('/');
+				}
+				else{
+					rejected(inputs[x]);
+			  		res.redirect('/');
+				}
+			}
+			else{
+				rejected.push(inputs[x]);
+			 	res.redirect('/');
+
+			}
+	  		check = false;	
+        }
 	}catch(e){
 		console.log(e);
 		next(e)
@@ -37,8 +67,8 @@ app.post('/addIP',async(req,res) =>{
 app.post('/removeIP',async(req,res) =>{
 	try{	    
 		var removeIP = req.body.check;
-		if (typeof completeTask === 'string') {
-			await knex('IP').where( {'IPadd':removeIP}).del();     
+		if (typeof removeIP === 'string') {
+			await knex('IP').where({'IPadd':removeIP}).del();     
     	} 
 
     	else if (typeof removeIP === 'object') {
@@ -69,7 +99,7 @@ app.get('/',async(req,res) =>{
         	
         };
         
-        res.render("index", { task: task});
+        res.render("index", { task: task, accepted: accepted, rejected :rejected});
     } catch (e) {
         console.log(e);
     }
